0b489f2c9d802fcbf7f05dfcfe004619368ce150
Marco <ma8155354@gmail.com>
2024-11-17 18:49:02 -0300

    Piano funcionando
    Piano funcionando conforme o esperado, resta apenas aplicar alguns conceitos para aprimorar a jogabilidade do teclado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,48 @@
 const pianosKeys = document.querySelectorAll(".piano-keys .keys")
+let mapedKeys = []
+const volume = document.querySelector(".volume input") 
+const keysCheck = document.querySelector(".keys-check input")
 
 let audio = new Audio("./src/tunes/z.mp3")
 
 const playTune = (key) => {
     audio.src = `./src/tunes/${key}.mp3`
     audio.play()
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    
+    clickedKey.classList.add("active")
+    
+    setTimeout(() =>{
+        clickedKey.classList.remove("active")
+    }, 150)
 };
+
 
 pianosKeys.forEach((key) => {
     key.addEventListener('click', ()=> playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
 })
 
 document.addEventListener('keydown', (event) => {
-    
-    playTune(event.key)
+    if(mapedKeys.includes(event.key)){
+        playTune(event.key)
+    }
+
 })
+
+const handleVolume = (event) => {
+    audio.volume = event.target.value
+    console.log(event.target.value)
+}
+
+const hideKeys = () => {
+    pianosKeys.forEach((key) => {
+        key.classList.toggle("hide")
+    })
+}
+
+volume.addEventListener("input", handleVolume)
+
+keysCheck.addEventListener("click", hideKeys)
+
